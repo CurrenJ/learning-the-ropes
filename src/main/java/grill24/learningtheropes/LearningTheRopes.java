6b0c3ecf5c7ae24e65bf87f988cdbdff40946a47
Curren <jeandell.curren@gmail.com>
2025-05-11 18:53:36 +0100

    Add blank mod

---
+++ src/main/java/grill24/learningtheropes/LearningTheRopes.java
@@ -0,0 +1,14 @@
+package grill24.learningtheropes;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(LearningTheRopes.MOD_ID)
+public class LearningTheRopes {
+    public static final String MOD_ID = "learningtheropes";
+
+    public LearningTheRopes(IEventBus eventBus, ModContainer modContainer) {
+
+    }
+}
